674fde443d61972640093cb345d1d6901d18516c
jiale <jiale8432@gmail.com>
2021-10-18 19:48:14 +0800

    homepage navigate to login

--- smartClassroom.py
+++ smartClassroom.py
@@ -8,8 +8,13 @@ app=Flask(__name__)
 @app.route("/homePage")
 @app.route("/")
 def getStart():
-    return render_template('homePage.html')
+    return render_template('homePage.html',title='Home')
 
+
+@app.route("/")
+@app.route("/loginPage")
+def login():
+    return render_template('loginPage.html',title='Login')
 
 if __name__=='__main__':
     app.run(debug=True) 
