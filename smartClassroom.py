1bb619903210c8b5ef2648e215055d38fc0644e7
alvinling120 <alvinling120@gmail.com>
2021-10-23 12:51:15 +0800

    Added multiple pages and images, not yet combined

--- smartClassroom.py
+++ smartClassroom.py
@@ -22,5 +22,25 @@ def mainMenu():
     return render_template('mainmenu.html',title='Main Menu')
 
 
+@app.route("/") 
+@app.route("/quizmenu")
+def quizMenu():
+    return render_template('quizmenu.html',title='Quiz Menu')
+
+@app.route("/") 
+@app.route("/createQuestion")
+def createQuizQuestion():
+    return render_template('createQuestion.html',title='Create Quiz Question')
+
+@app.route("/") 
+@app.route("/questionSummary")
+def questionSummary():
+    return render_template('questionSummary.html',title='Question Summary')
+
+@app.route("/") 
+@app.route("/reportSummary")
+def reportSummary():
+    return render_template('reportSummary.html',title='Report Summary')
+
 if __name__=='__main__':
     app.run(debug=True) 
