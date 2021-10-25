8566a4e817e747af5b81922b0d1f05517479cb90
jiale <jiale8432@gmail.com>
2021-10-25 18:12:03 +0800

    button page interaction combine

--- smartClassroom.py
+++ smartClassroom.py
@@ -11,41 +11,41 @@ def getStart():
     return render_template('homePage.html',title='Home')
 
 
-@app.route("/")
+##@app.route("/")
 @app.route("/loginPage")
 def login():
     return render_template('account_module/loginPage.html',title='Login')
 
 
-@app.route("/")
+##@app.route("/")
 @app.route("/registerPage")
 def register():
     return render_template('account_module/registerPage.html',title='Register')
 
-@app.route("/loginPage")
+##@app.route("/loginPage")
 @app.route("/mainMenu")
 def mainMenu():
     return render_template('mainmenu.html',title='Main Menu')
   
 
 
-@app.route("/") 
-@app.route("/quizmenu")
+##@app.route("/mainMenu") 
+@app.route("/mainMenu/quizmenu")
 def quizMenu():
     return render_template('quizmenu.html',title='Quiz Menu')
 
-@app.route("/") 
-@app.route("/createQuestion")
+##@app.route("/") 
+@app.route("/mainMenu/quizmenu/questionSummary/createQuestion")
 def createQuizQuestion():
     return render_template('createQuestion.html',title='Create Quiz Question')
 
-@app.route("/") 
-@app.route("/questionSummary")
+##@app.route("/") 
+@app.route("/mainMenu/quizmenu/questionSummary")
 def questionSummary():
     return render_template('questionSummary.html',title='Question Summary')
 
-@app.route("/") 
-@app.route("/reportSummary")
+##@app.route("/") 
+@app.route("/mainMenu/quizmenu/reportSummary")
 def reportSummary():
     return render_template('reportSummary.html',title='Report Summary')
 
