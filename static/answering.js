3b66caeeb1c13c39a6e9b0d5974d70061f717a0c
alvinling120 <alvinling120@gmail.com>
2021-11-13 19:32:01 +0800

    add timer

--- static/answering.js
+++ static/answering.js
@@ -5,9 +5,28 @@ document.onreadystatechange = function() {
         document.querySelector(
             ".spinner-grow").style.visibility = "visible";
     } else {
+        startCount=TRUE;
         document.querySelector(
             ".spinner-grow").style.display = "none";
         document.querySelector(
             "div").style.visibility = "visible";
     }
 };
+
+
+        var counter = 10;
+        var startCount = FALSE
+
+        if (startCount === TRUE) {
+        setInterval(function(){ 
+            counter--;
+            if (counter >=0){
+                id = document.getElementById("count");
+                id.innerHTML = counter;
+            }
+            if(counter ==0){
+                alert("Time is up!");
+            }
+        
+        },1000);
+    }
